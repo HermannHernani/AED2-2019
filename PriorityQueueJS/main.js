f9d4fed3486d9bef57b7935e1d1e9658af8f5617
Carol Bastos <acbl.ads@uea.edu.br>
2019-04-24 22:43:09 -0400

    Fila de prioridades com JS

---
+++ PriorityQueueJS/main.js
@@ -0,0 +1,23 @@
+
+let pQueue = new PriorityQueue();
+
+
+pQueue.enqueue('Queila', 6);
+pQueue.enqueue('Ana', 9);
+pQueue.enqueue('Camila', 7);
+pQueue.enqueue('Jack', 1);
+pQueue.enqueue('Joao', 3);
+pQueue.enqueue('Pedro', 4);
+pQueue.enqueue('Maria', 5);
+
+
+//pQueue.dequeue();
+//pQueue.dequeue();
+//pQueue.dequeue();
+//pQueue.dequeue();
+pQueue.print();
+
+console.log(pQueue.isEmpty());
+
+
+
